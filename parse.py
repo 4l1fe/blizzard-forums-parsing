19c641fe4d23ba5fbd76fc3df78d54be34b835b8
dim <dim@dim>
2015-12-20 18:03:10 +0300

    разможены корутины на отправку запросов, сихнорнизация и завершение через очередь, добавлен файл зависимостей

--- parse.py
+++ parse.py
@@ -4,48 +4,73 @@ import time
 import fake_useragent
 import tornado.ioloop
 from bs4 import BeautifulSoup
-from tornado.httpclient import AsyncHTTPClient, HTTPClient
+from tornado.httpclient import AsyncHTTPClient
 from tornado.gen import coroutine
+from tornado.queues import Queue
 
 
 logger = logging.getLogger('')
 formatter = logging.Formatter(style='{', datefmt='%H:%M:%S', fmt='[{asctime}.{msecs:.3g} {levelname}] {message}')
 stream_hndl = logging.StreamHandler(stream=sys.stdout)
 stream_hndl.setFormatter(formatter)
-stream_hndl.setLevel(logging.DEBUG)
 logger.addHandler(stream_hndl)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 
 @coroutine
-def main():
-    url = 'http://eu.battle.net/hearthstone/ru/blog/infinite?page={}&articleType=blog'
-    page = 1
-    # client = AsyncHTTPClient()
-    client = HTTPClient()
-    file = open('news.html', 'a')
-
-    start_time = time.time()
+def writer(queue):
     while True:
-        user_agent = fake_useragent.UserAgent(cache=True).random #todo снести кэш-файл
-        try:
-            # response = yield client.fetch(url.format(page), user_agent=user_agent)
-            response = client.fetch(url.format(page), user_agent=user_agent)
-            if not response.body:
-                break
-            logger.info('Request time - {}'.format(response.request_time))
-            soup = BeautifulSoup(response.body.decode(), 'html.parser')
-            file.write(soup.prettify())
-            page += 1
-        except:
-            logger.exception()
-            break
+        data = yield queue.get()
+        if data == 'start':
+            logger.info('Writer is started')
+        else:
+            logger.info('Data is gotten')
+            soup = BeautifulSoup(data.decode(), 'html.parser')
+            with open('news.html', 'a') as file:
+                file.write(soup.prettify())
+            logger.info('Data is written')
 
-    file.close()
+            queue.task_done()
+            logger.info('Task done')
+
+page = 1
+alive = 8
+@coroutine
+def main():
+    queue = Queue()
+    url = 'http://eu.battle.net/hearthstone/ru/blog/infinite?page={}&articleType=blog'
+    start_time = time.time()
+
+    @coroutine
+    def fetcher(i):
+        logger.info('Fetcher {} is started'.format(i))
+        global page, alive
+        while True:
+            client = AsyncHTTPClient()
+            user_agent = fake_useragent.UserAgent(cache=True).random #todo снести кэш-файл
+            response = yield client.fetch(url.format(page), user_agent=user_agent)
+            if response.body:
+                yield queue.put(response.body)
+                logger.info("{} Page's request time - {}".format(page, response.request_time))
+                page += 1
+            else:
+                logger.info('No data. Fetcher {} is stoped'.format(i))
+                alive -= 1
+                if not alive:
+                    queue.task_done()
+                break
+
+    for i in range(alive):
+        fetcher(i)
+
+    writer(queue)
+    queue.put('start')
+
+    yield queue.join()
     end_time = time.time()
-    duration = int(end_time-start_time)
-    logger.info('Duration - {}, Pages - {}'.format(duration, page))
+    logger.info('Duration - {}'.format(end_time-start_time))
 
 
 if __name__ == '__main__':
+    AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient") # использует keep-alive
     tornado.ioloop.IOLoop.current().run_sync(main)
