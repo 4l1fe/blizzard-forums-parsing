09a9703179039c1a6625073de353e6383af40d68
4l1fe <4l1fe@users.noreply.github.com>
2015-12-25 02:11:18 +0300

    добавлены опции использования curl, lxml

--- parse_forum.py
+++ parse_forum.py
@@ -16,30 +16,34 @@ logger.addHandler(stream_hndl)
 logger.setLevel(logging.INFO)
 
 
-def main(fetcher_count, fetcher_concurrent, worker_count):
+def main(args):
     url_queue, data_queue = JoinableQueue(), JoinableQueue() #todo maxsize
+    url_cache = set() # todo: обезопасить доступ мьютексом?
     logger.info('Main[pid={}] process is started'.format(os.getpid()))
+    logger.info('Args: {}'.format(args))
 
-    logger.info('Start {} fetchers'.format(fetcher_count))
+    logger.info('Start {} fetchers'.format(args.fetcher_count))
 
     fetchers = []
-    for i in range(fetcher_count):
-        p = Process(target=fetcher, name='Fetcher', args=(url_queue, data_queue, fetcher_concurrent))
+    for i in range(args.fetcher_count):
+        p = Process(target=fetcher, name='Fetcher', args=(url_queue, data_queue, args.fetcher_concurrent, args.use_curl))
         p.start()
         fetchers.append(p)
 
-    logger.info('Start {} workers'.format(worker_count))
+    logger.info('Start {} workers'.format(args.worker_count))
     workers = []
-    for i in range(worker_count):
-        p = Process(target=worker, name='Worker', args=(url_queue, data_queue))
+    for i in range(args.worker_count):
+        p = Process(target=worker, name='Worker', args=(url_queue, data_queue, url_cache, args.use_lxml))
         p.start()
         workers.append(p)
 
     logger.info('=================================Start parsing=================================')
     start_time = time.time()
     url_queue.put('http://eu.battle.net/hearthstone/ru/forum/10362657/')
-    url_queue.join()
-    data_queue.join()
+    # url_queue.join()
+    # logger.info('Url queue is empty')
+    # data_queue.join()
+    # logger.info('Data queue is empty')
     end_time = time.time()
     logger.info('End parsing. Duration - {}'.format(end_time-start_time))
 
@@ -49,6 +53,8 @@ if __name__ == '__main__':
     parser.add_argument('--fetcher-count', type=int, default=1)
     parser.add_argument('--fetcher-concurrent', type=int, default=5)
     parser.add_argument('--worker-count', type=int, default=1)
-    args = parser.parse_args()
+    parser.add_argument('--use-curl', action='store_true')
+    parser.add_argument('--use-lxml', action='store_true')
+    args = parser.parse_args(['--use-curl', '--use-lxml'])
 
-    main(args.fetcher_count, args.fetcher_concurrent, args.worker_count)
+    main(args)
