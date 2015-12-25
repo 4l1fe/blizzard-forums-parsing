884bed44f6aea52129610a50cbb02bdf6b524029
dim <dim@dim>
2015-12-25 20:42:37 +0300

    добавлен парсинг поста, рефакторинг логирования

--- fetcher.py
+++ fetcher.py
@@ -15,7 +15,6 @@ def fetcher(url_queue, data_queue, fetcher_concurrent, use_curl):
     pid = os.getpid()
     f_id = 'Fetcher[pid={}]'.format(pid)
     logger.info('{} is started.'.format(f_id))
-    logger.info('{} qsize {}'.format(f_id, url_queue.qsize()))
     pool = TPE(fetcher_concurrent)
 
     @coroutine
@@ -25,19 +24,19 @@ def fetcher(url_queue, data_queue, fetcher_concurrent, use_curl):
         def fetch():
             while True:
                 url = yield pool.submit(url_queue.get)
-                logger.info('{} got url {}'.format(f_id, url))
+                logger.debug('{} got url {}'.format(f_id, url))
                 client = AsyncHTTPClient()
                 user_agent = fake_useragent.UserAgent(cache=True).random #todo снести кэш-файл
                 response = yield client.fetch(url, user_agent=user_agent)
                 if response.body:
                     yield pool.submit(data_queue.put, (url, response.body))
-                    logger.info("{} request time - {}. {}".format(f_id, response.request_time, url))
+                    logger.debug("{} request time - {}. {}".format(f_id, response.request_time, url))
                 else:
                     logger.info('{} is stoped. No data.'.format(f_id))
                     break
                 url_queue.task_done()
 
-        logger.info('{} starts {} requests'.format(f_id, fetcher_concurrent))
+        logger.info('{} starts {} concurrent requests'.format(f_id, fetcher_concurrent))
         for _ in range(fetcher_concurrent):
             fetch()
 
