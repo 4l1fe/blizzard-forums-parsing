cbda0fac29c7e301c3be4ca8d2f73f2b24107dd5
4l1fe <4l1fe@users.noreply.github.com>
2016-09-06 21:50:15 +0300

    Проба починки 599: Operation timed out after 20000 milliseconds

--- fetcher.py
+++ fetcher.py
@@ -30,36 +30,35 @@ def fetcher(options, fetcher_concurrent, stop_flag, use_curl=False):
     logger.info('Fetcher is started. Redis connection {}:{}'.format(options.redis_host, options.redis_port))
 
     @coroutine
+    def fetch(i):
+        tr_client = tornadoredis.Client(connection_pool=pool)
+        while not stop_flag.value:
+            try:
+                url = yield Task(tr_client.brpop, cns.URL_QUEUE_KEY, cns.BLOCKING_TIMEOUT)
+                url = url[cns.URL_QUEUE_KEY]
+                logger.debug('Coroutine {} got url {}'.format(i, url))
+                client = AsyncHTTPClient()
+                user_agent = fake_useragent.UserAgent(cache=True).random # кэш во временной папке системы
+                response = yield client.fetch(url, user_agent=user_agent)
+                if response.body:
+                    data_key = cns.DATA_KEY_PREFIX + url
+                    pipeline = tr_client.pipeline(transactional=True)
+                    pipeline.lpush(cns.DATA_QUEUE_KEY, data_key)
+                    pipeline.set(data_key, response.body.decode())
+                    yield Task(pipeline.execute)
+                    logger.debug("Coroutine {} request time: {}. {}".format(i, response.request_time, url))
+            except:
+                logger.exception('Coroutine {} error with url {}'.format(i, url))
+        logger.info('Coroutine {} is stopped'.format(i))
+
+    @coroutine
     def main():
-
-        @coroutine
-        def fetch(i):
-            tr_client = tornadoredis.Client(connection_pool=pool)
-            while not stop_flag.value:
-                try:
-                    url = yield Task(tr_client.brpop, cns.URL_QUEUE_KEY, cns.BLOCKING_TIMEOUT)
-                    url = url[cns.URL_QUEUE_KEY]
-                    logger.debug('Coroutine {} got url {}'.format(i, url))
-                    client = AsyncHTTPClient()
-                    user_agent = fake_useragent.UserAgent(cache=True).random # кэш во временной папке системы
-                    response = yield client.fetch(url, user_agent=user_agent,
-                                                  request_timeout=cns.REQUEST_TIMEOUT,
-                                                  connect_timeout=cns.CONNECT_TIMEOUT)
-                    if response.body:
-                        data_key = cns.DATA_KEY_PREFIX + url
-                        pipeline = tr_client.pipeline(transactional=True)
-                        pipeline.lpush(cns.DATA_QUEUE_KEY, data_key)
-                        pipeline.set(data_key, response.body.decode())
-                        yield Task(pipeline.execute)
-                        logger.debug("Coroutine {} request time: {}. {}".format(i, response.request_time, url))
-                except:
-                    logger.exception('Coroutine {} error with url {}'.format(i, url))
-            logger.info('Coroutine {} is stopped'.format(i))
-
         logger.info('Start {} concurrent requests'.format(fetcher_concurrent))
-        completed = yield [fetch(i) for i in range(1, fetcher_concurrent+1)]
+        yield [fetch(i) for i in range(1, fetcher_concurrent+1)]
 
     if use_curl:
-        AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient") # использует keep-alive
+        AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient", defaults=dict(max_clients=fetcher_concurrent,
+                                                                                               request_timeout=cns.REQUEST_TIMEOUT,
+                                                                                               connect_timeout=cns.CONNECT_TIMEOUT))
     tornado.ioloop.IOLoop.current().run_sync(main)
     logger.info('Stopped. IOloop shutdown.')
